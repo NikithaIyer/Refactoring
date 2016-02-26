cd95ac54c6a49b7739e76e92f6a96ce30a4df62a
arathyjan <arathyjanardhanan@gmail.com>
2016-02-26 17:12:03 +0530

    Replace temp value with query

--- src/main/java/com/thoughtworks/movierental/Customer.java
+++ src/main/java/com/thoughtworks/movierental/Customer.java
@@ -25,10 +25,8 @@ class Customer {
     Enumeration rentals = _rentals.elements();
     String result = "Rental Record for " + getName() + "\n";
     while (rentals.hasMoreElements()) {
-      double thisAmount = 0;
       Rental each = (Rental) rentals.nextElement();
       //determine amounts for each line
-      thisAmount = each.getCharge();
       // add frequent renter points
       frequentRenterPoints++;
       // add bonus for a two day new release rental
@@ -38,8 +36,8 @@ class Customer {
 
       //show figures for this rental
       result += "\t" + each.getMovie().getTitle() + "\t" +
-          String.valueOf(thisAmount) + "\n";
-      totalAmount += thisAmount;
+          String.valueOf(each.getCharge()) + "\n";
+      totalAmount += each.getCharge();
     }
     //add footer lines result
     result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
